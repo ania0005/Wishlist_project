fd90b59dba5f3c98fc4348e4475f82c020e589f8
ania0005 <ania000535@gmail.com>
2024-05-10 01:25:21 +0200

    add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import Header from "../components/header/Header";
-import Footer from "../components/footer/Footer";
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Предполагается, что вы используете React Router для навигации
+
+
 import photo03 from "./../img/Photo03.png";
+import gift from "./../img/gift.png"
 import "../App.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleCreateWishlistClick = () => {
+    navigate('/login');
+  };
+
   return (
     <>
       <Header />
@@ -11,8 +21,21 @@ const Home = () => {
         <div className="container">
           <img src={photo03} alt="" className="project-instruction" />
         </div>
+
+        <div className="purple-box">
+          <div className="content-left">
+            <h2 className="title">Receive only the gifts you want!</h2>
+            <p>No stupid or ridiculous surprises! Add a link and be guaranteed to get what you want in the right form, color, and quality!</p>
+            <Link to="/signup">
+              <button>I want a Wishlist</button>
+            </Link>
+          </div>
+          <div className="content-right">
+            <img src={gift} alt="Your Alt Text" />
+          </div>
+        </div>
+
       </main>
-      
     </>
   );
 };
