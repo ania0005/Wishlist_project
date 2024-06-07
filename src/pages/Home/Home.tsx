24efdfaa4e1eb4161029d051a6a6e1f88621768c
Tania Samoilenko <80690310+ania0005@users.noreply.github.com>
2024-06-07 23:52:29 +0200

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,13 @@
-import { useContext } from "react";
 import Header from "../../components/header/Header";
-import { Link } from "react-router-dom";
 import share from "../../img/share.jpg";
 import info from "../../img/info.jpg";
 import gift from "../../img/gift.png";
 import add from "../../img/add.jpg";
-import AuthContext from "../../contexts/AuthContext";
 import "./Home.css";
 
-
 const Home = () => {
-  const { user} = useContext(AuthContext);
-
   return (
-    <>
+    <div className="Home">
       <Header />
       <main className="section">
         <div className="pink-box">
@@ -21,73 +15,58 @@ const Home = () => {
           <div className="columns-container">
             <div className="column">
               <div className="image-container">
-                <img src={add} alt="Image 1" />
+                <img src={add} alt="Create a wish list" />
               </div>
               <div className="white-rectangle-text">
-                STEP 1<br />               
-                 <br />
-                <strong>Create a wish list</strong>
+                STEP 1
                 <br />
+                <strong>Create a wish list</strong>
                 <br />
                 Add the gifts you need with links to online store
               </div>
             </div>
             <div className="column">
               <div className="image-container">
-                <img src={share} alt="Image 2" />
+                <img src={share} alt="Share with friends" />
               </div>
               <div className="white-rectangle-text">
-                STEP 2<br />               
-                 <br />
+                STEP 2
+                <br />
                 <strong>Share with your friends</strong>
                 <br />
-                <br />
-                Send them a link and everyone can reserve a gift from the list{" "}
+                Send them a link and everyone can reserve a gift from the list
               </div>
             </div>
             <div className="column">
               <div className="image-container">
-                <img src={info} alt="Image 3" />
+                <img src={info} alt="Enjoy your gifts" />
               </div>
               <div className="white-rectangle-text">
-                STEP 3<br />
+                STEP 3
                 <br />
                 <strong>Ready! Enjoy!</strong>
                 <br />
-                <br />
-                You get cool gifts. Your friends don’t ask the question
-                “What should I give?”{" "}
+                You get cool gifts. Your friends don’t ask the question “What should I give?”
               </div>
             </div>
           </div>
         </div>
-
         <div className="purple-box">
           <div className="content-left">
             <h2 className="title">Receive only the gifts you want!</h2>
             <p>
-              {" "}
               Avoid any unwanted or absurd surprises! Simply provide a link and
               ensure you receive exactly what you desire, in the perfect form,
               color, and quality!
             </p>
-
-            {user ? (
-              <Link to="/createWishlist">
-                <button>I want a Wishlist</button>
-              </Link>
-            ) : (
-              <Link to="/login">
-                <button>I want a Wishlist</button>
-              </Link>
-            )}
+            <button>I want a Wishlist</button>
           </div>
           <div className="content-right">
-            <img src={gift} alt="Your Alt Text" />
+            <img src={gift} alt="Gift" />
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
