164919259fb8d3921094edb325f2f89a52389945
Bobkova Maryna <marih@bk.ru>
2024-06-11 00:19:34 +0200

    mobi

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,16 +7,17 @@ const Footer = () => {
       <div className="footer__wrapper">
         <ul className="footer__left">
           <li>© 2024 Made with love for good gifts</li>
-          <li>Cohort-34</li>
+       
         </ul>
         
         <ul className="github_logo">
         <li className='privacy-policy'><a href="/privacy-policy">Privacy Policy</a></li>
-        <li className="social__item"><a href="https://github.com/ania0005/Wishlist_project" target="_blank"><img src={gitHub} alt="Link"/></a></li>
         
         <ul className="footer__right">
           
-          <li>Linkstraße 2/8 Etage,<br/>10785 Berlin<br/>AIT-TR</li>
+          <li>Linkstraße 2/8 Etage, 10785 Berlin, AIT-TR</li>
+          <li className="social__item"><a href="https://github.com/ania0005/Wishlist_project" target="_blank"><img src={gitHub} alt="Link"/></a></li>
+
         </ul> </ul> 
       </div>
     </footer>
